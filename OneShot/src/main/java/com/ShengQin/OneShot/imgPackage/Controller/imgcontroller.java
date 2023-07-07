8c1608957c36d263a96428a3a1fb8bf1153de155
Minghao Han <3508627758@qq.com>
2023-07-07 08:29:34 +0800

    修改拦截器

--- OneShot/src/main/java/com/ShengQin/OneShot/imgPackage/Controller/imgcontroller.java
+++ OneShot/src/main/java/com/ShengQin/OneShot/imgPackage/Controller/imgcontroller.java
@@ -2,11 +2,14 @@ package com.ShengQin.OneShot.imgPackage.Controller;
 import com.ShengQin.OneShot.imgPackage.Utils.uploadutilgame;
 import com.ShengQin.OneShot.imgPackage.Utils.uploadutilshot;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import java.io.IOException;
+import java.util.Map;
 
 
 @RestController
@@ -16,8 +19,12 @@ public class imgcontroller {
 
     //shot图片仓库
     @PostMapping("/upimgshot")
-    public String upimgshot(MultipartFile file,int shotid) throws IOException {
-        return uploadutilshot.uploadImageshot(file,shotid);
+    public String upimgshot(MultipartHttpServletRequest multipartRequest) throws IOException {
+//        int shotid = (int) requestBody.get("shotid");
+        MultipartFile file = multipartRequest.getFile("file");
+        uploadutilshot.uploadImageshot(file,4);
+        System.out.println(file);
+        return "success";
     }
 
     //比赛图片仓库
